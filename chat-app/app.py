ed7168c1c10c4779703c1d04a38e2b8d0b8a86f9
marcus4urelius <marcabraham2k7@gmail.com>
2025-05-01 11:38:10 +0400

    Update app.py

--- chat-app/app.py
+++ chat-app/app.py
@@ -15,6 +15,9 @@ def handleMessage(msg):
     send(msg, broadcast=True)
 
 import os
-port = int(os.environ.get("PORT", 10000))
-socketio.run(app, host='0.0.0.0', port=port)
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))  # Use PORT from Render
+    socketio.run(app, host='0.0.0.0', port=port)
+
 
