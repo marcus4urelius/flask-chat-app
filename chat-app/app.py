2ba260ade3b291a0e20c3f6b610a7b7784d1b96f
marcus4urelius <marcabraham2k7@gmail.com>
2025-05-01 11:42:47 +0400

    Update3 app.py

--- chat-app/app.py
+++ chat-app/app.py
@@ -1,23 +1,17 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO, send
+from flask_socketio import SocketIO
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
+app.config['SECRET_KEY'] = 'secret'
 socketio = SocketIO(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@socketio.on('message')
-def handleMessage(msg):
-    print(f"Message: {msg}")
-    send(msg, broadcast=True)
-
-import os
+# Add any other events or routes here...
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Use PORT from Render
+    port = int(os.environ.get("PORT", 5000))
     socketio.run(app, host='0.0.0.0', port=port)
-
-
