e6af9a47568bc62b8f4e5d96abc6b8c837251eeb
marcus4urelius <marcabraham2k7@gmail.com>
2025-05-02 09:42:16 +0400

    Update 5app.py

--- chat-app/app.py
+++ chat-app/app.py
@@ -10,14 +10,7 @@ socketio = SocketIO(app)
 def index():
     return render_template('index.html')
 
-# Add any other events or routes here...
-
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    import eventlet
-    import eventlet.wsgi
-
-    if __name__ == '__main__':
-        port = int(os.environ.get("PORT", 5000))
-        eventlet.wsgi.server(eventlet.listen(('0.0.0.0', port)), app)
+    socketio.run(app, host='0.0.0.0', port=port)
 
